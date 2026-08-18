41eb9167f7d3d3253b4d49ccdb9c983dc3eefcfd
Ysaac Andrade <yseshow08@gmail.com>
2026-08-18 12:39:42 -0300

    Exercices of pytho

---
+++ python-execises/test.py
@@ -0,0 +1,3 @@
+print("Hello, World!")
+print("The sum of 2 + 2 is ", 2 + 2)
+print("Everything is ready to start programming!")
---
+++ python-execises/whoami.py
@@ -0,0 +1,3 @@
+print("Name: Ysaac")
+print("City: Campinas")
+print("Because I love computers and the art of create things")
